a904bae775264f047ba168df88d6604942399931
Theresa Skonieczny <tsscamaro@gmail.com>
2017-05-16 19:47:57 -0400

    added table of products with delete and update actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LabTwenty.Models;
 
 namespace LabTwenty.Controllers
 {
@@ -31,23 +32,104 @@ namespace LabTwenty.Controllers
             ViewBag.Message = "Welcome to GC COFFEE!";
             return View();
         }
-        public ActionResult AddUser(string FirstName, string LastName, string Email, string Phone, string Password)
+        public ActionResult AddUser(UserInfo NewUser)
         {
-            string fName = FirstName;
-            ViewBag.FirstName = "Your registered" + "" + fName;
+            // VALIDATION ! //
+            //USED TO CALL/ADD DATA FROM THE MODEL to the DATABASE//
 
-            //string pass1 = "";
-            //Console.Write(pass1.PadLeft(5, '*'));
-            
-
-            return View();
-            
-
-            
+            return View(NewUser);//pass this NewUser model to the AddUser View     
 
             //code to get user information
+            //string fName = FirstName;
+            //ViewBag.FirstName = "Your registered " + fName;
+
+            ////string pass1 = "";
+            ////Console.Write(pass1.PadLeft(5, '*'));
         }
-            
+        public ActionResult Admin()
+        {
+            ViewBag.Message = "Administration";
+
+            CoffeeShopDBEntities CS = new CoffeeShopDBEntities();
+
+            //select * from customers
+            List<Item> ItemList = CS.Items.ToList();
+
+            ViewBag.ItemList = ItemList;
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewItem(Item NewItem)
+        {
+            CoffeeShopDBEntities CS = new CoffeeShopDBEntities();
+
+            CS.Items.Add(NewItem);
+
+            CS.SaveChanges();
+
+            return RedirectToAction("Admin");
+        }
+
+        public ActionResult Delete(string Name)
+        {
+            CoffeeShopDBEntities CS = new CoffeeShopDBEntities();
+
+            //step #1 "find the custoemr that I need to delete....//
+            Item ToDelete = CS.Items.Find(Name);
+
+            if (ToDelete == null)
+            {
+                ViewBag.ErrorMessage = "Unavailable";
+                return View("ErrorMessage");
+            }
+
+            //removes OBJECTS from DATABASE
+
+            CS.Items.Remove(ToDelete);
+
+            //to SAVE EVERYTHING TO DATABASE.....
+            CS.SaveChanges();
+
+            return RedirectToAction("Admin");
+        }
+        public ActionResult Update(string Name)
+        {
+            //first thing...TO SHOW DATA
+
+            CoffeeShopDBEntities CS = new CoffeeShopDBEntities();
+
+            //update will SHOW up & THE MODEL will be updated......
+            Item ToFind = CS.Items.Find(Name);
+
+            return View("Edit", ToFind);
+        }
+
+
+        public ActionResult SaveUpdates(Item ToBeUpdated)
+        {
+            CoffeeShopDBEntities CS = new CoffeeShopDBEntities();
+        //find the original customer record
+        Item ToFind = CS.Items.Find(ToBeUpdated);
+
+            ToFind.Name = ToBeUpdated.Name;
+
+            ToFind.Description = ToBeUpdated.Description;
+
+            ToFind.Quantity = ToBeUpdated.Quantity;
+
+            ToFind.Price = ToBeUpdated.Price;
+
+            CS.SaveChanges();
+            return RedirectToAction("Admin");
 
     }
+
+
+}
 }
---
+++ Models/UserInfo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabTwenty.Models
+{
+    public class UserInfo
+    {
+        private string FirstName;
+        private string LastName;
+        private string Email;
+        private string Phone;
+        private string Password;
+
+        public string UserFirstName 
+        {
+            get
+            {
+                return FirstName;
+            }
+
+            set
+            {
+                FirstName = value;
+            }
+        }
+
+        public string UserLastName          
+        {
+            get
+            {
+                return LastName;
+            }
+
+            set
+            {
+                LastName = value;
+            }
+        }
+
+        public string UserEmail
+        {
+            get
+            {
+                return Email;
+            }
+
+            set
+            {
+                Email = value;
+            }
+        }
+
+        public string UserPhone
+        {
+            get
+            {
+                return Phone;
+            }
+
+            set
+            {
+                Phone = value;
+            }
+        }
+
+        public string UserPassword
+        {
+            get
+            {
+                return Password;
+            }
+
+            set
+            {
+                Password = value;
+            }
+        }
+        public UserInfo():this("","","","","")
+        {
+            //FirstName = "";
+            //LastName = "";
+            //Email = "";
+            //Phone = "";
+            //Password = "";
+        }
+
+        public UserInfo(string firstName, string lastName, string email, string phone, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+    }
+}
